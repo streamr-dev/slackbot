efaf778b134d11af6fe21c0e638fb996173946cc
juslesan <santeri.juslenius@gmail.com>
2021-10-01 15:23:19 +0300

    add palin text info

--- slackbot.js
+++ slackbot.js
@@ -32,6 +32,15 @@ class SlackBot {
         }
     }
 
+    info(message, header='') {
+        const info = `*${header} Info:*\n${message}` 
+        try {
+            this.client.chat.postMessage({channel: this.channel, text: info})
+        } catch (err) {
+            console.error('Failed to send info to channel', this.channel)
+        }
+    }
+
     dateTimeNow() {
         const date = new Date()
         return date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2) + "T" + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) + ":" + ("0" + date.getSeconds()).slice(-2) + " | "
